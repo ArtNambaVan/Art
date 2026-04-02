809613798e16a96b134691d5652852765bd76573
Artem Klochkov <aklochkov@atypicaldigital.com>
2026-04-02 15:25:27 +0400

    add assets

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import type { ReactElement } from 'react';
 
 export default function Home(): ReactElement {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className='h1-bold'>TEst</h1>
+    <div className="">
+      <h1 className='body-regular'>ABCD</h1>
+      <h1 className='body-regular font-space-grotesk'>ABCD</h1>
     </div>
   );
 }
